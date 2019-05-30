289f599938eb4b2402fdd5581b1a2b01d824cba0
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-05-30 16:53:48 -0400

    added keys and process env file

--- liri.js
+++ liri.js
@@ -1,5 +1,26 @@
+
+
+//require secret keys
 require("dotenv").config();
+
+//npm packages
+const chalk = require('chalk');
+const yargs = require('yargs');
+const axios = require('axios');
+const moment = require('moment');
+//api keys and key file
 const keys = require('./keys');
+var spotify = new spotify(keys.spotify);
+//spotify api
+const spotifyAPI = require("node-spotify-api");
+const fs = require("fs");
+
+
+const spotify = new spotify(keys.spotify)
+
+
+
+
  
 
 
